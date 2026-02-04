b5074d66385b2e06f0a1becd922256a5eb11e140
Aniket Maurya <theaniketmaurya@gmail.com>
2026-02-04 22:54:39 +0000

    fix

--- astro.config.ts
+++ astro.config.ts
@@ -15,6 +15,7 @@ import { SITE } from "./src/config";
 // https://astro.build/config
 export default defineConfig({
   site: SITE.website,
+  base: "/blog",
   trailingSlash: "always",
   output: "server",
   adapter: cloudflare(),
